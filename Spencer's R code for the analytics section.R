33c661a6d49adf62c9b90e6d6351e1efb31b06c9
Spencer <spencermstarr@hotmail.com>
2021-04-28 15:05:22 -0700

    Add files via upload

--- Spencer's R code for the analytics section.R
+++ Spencer's R code for the analytics section.R
@@ -7,6 +7,7 @@ setwd('C:/Users/spenc/OneDrive/Documents/George Mason University/Spring 2021/CS 
 library(plm)
 library(tidyverse)
 library(rstatix)
+library(fBasics)
 library(coin)
 library(lubridate)
 library(RMariaDB)
@@ -19,32 +20,7 @@ dbConn <- dbConnect(RMariaDB::MariaDB(),default.file=mysqldb.connStrfile,group=m
 dbListTables(dbConn)
 
 
-# import the half hourly table 
-hhblocks <- dbGetQuery(dbConn, 
-                       "
-select hh1.day, hh2.numbSmMtrs, hh_0, hh_1, hh_2, hh_3, hh_4, hh_5, hh_6, hh_7, hh_8, hh_9, hh_10, hh_11, hh_12, hh_13, hh_14, hh_15, hh_16, hh_17, hh_18, hh_19, hh_20, hh_21, hh_22, hh_23, hh_24, hh_25, hh_26, hh_27, hh_28, hh_29, hh_30, hh_31, hh_32, hh_33, hh_34, hh_35, hh_36, hh_37, hh_38, hh_39, hh_40, hh_41, hh_42, hh_43, hh_44, hh_45, hh_46, hh_47
-from 
-(
-    select day, sum(hh_0) as hh_0, sum(hh_1) as hh_1,  sum(hh_2) as hh_2,  sum(hh_3) as hh_3,  sum(hh_4) as hh_4,  sum(hh_5) as hh_5,  sum(hh_6) as hh_6,  sum(hh_7) as hh_7,  sum(hh_8) as hh_8,  sum(hh_9) as hh_9,  sum(hh_10) as hh_10,  sum(hh_11) as hh_11,  sum(hh_12) as hh_12,  sum(hh_13) as hh_13,  sum(hh_14) as hh_14,  sum(hh_15) as hh_15,  sum(hh_16) as hh_16,  sum(hh_17) as hh_17,  sum(hh_18) as hh_18,  sum(hh_19) as hh_19,  sum(hh_20) as hh_20,  sum(hh_21) as hh_21,  sum(hh_22) as hh_22,  sum(hh_23) as hh_23,  sum(hh_24) as hh_24,  sum(hh_25) as hh_25,  sum(hh_26) as hh_26,  sum(hh_27) as hh_27,  sum(hh_28) as hh_28,  sum(hh_29) as hh_29,  sum(hh_30) as hh_30,  sum(hh_31) as hh_31,  sum(hh_32) as hh_32,  sum(hh_33) as hh_33,  sum(hh_34) as hh_34,  sum(hh_35) as hh_35,  sum(hh_36) as hh_36,  sum(hh_37) as hh_37,  sum(hh_38) as hh_38,  sum(hh_39) as hh_39,  sum(hh_40) as hh_40,  sum(hh_41) as hh_41,  sum(hh_42) as hh_42,  sum(hh_43) as hh_43,  sum(hh_44) as hh_44,  sum(hh_45) as hh_45,  sum(hh_46) as hh_46,  sum(hh_47) as hh_47
-    from hhblock
-    group by day
-) as hh1
-left outer join
-(
-    select day, count(distinct LCLid) as numbSmMtrs
-    from hhblock
-    group by day
-) as hh2
-on hh1.day = hh2.day
-;
-"
-)
 
-head(hhblocks)
-class(hhblocks)
-
-
-# import the tariffs table
 
 
 
@@ -60,7 +36,6 @@ str(dd)
 
 # import the household information table
 hi <- dbGetQuery(dbConn, "SELECT * FROM informations_households; ")
-
 str(hi)
 
 
@@ -75,6 +50,10 @@ str(dd_hi)
 
 # change the datetime column to a date column
 dd_hi$day <- ymd(dd_hi$day)
+
+# add a numerical ID column
+dd_hi$id <- substr(dd_hi$LCLid, 4, 9)
+dd_hi$id
 
 # add a dummy variable column for time of use houses
 dd_hi$ToU_dummy <- ifelse(dd_hi$stdorToU == "ToU", 1, 0)
@@ -133,93 +112,63 @@ categories3
 
 
 
+
+
 dd_hi_ToU <- dbGetQuery(dbConn, "SELECT * FROM daily_dataset
 LEFT JOIN informations_households ON daily_dataset.LCLid = informations_households.LCLid
 WHERE informations_households.stdorToU = 'ToU' AND energy_sum IS NOT NULL; ")
+class(dd_hi_ToU)
 
 median(dd_hi_ToU$energy_sum)
-class(dd_hi_ToU)
 median(dd_hi_ToU$energy_max)
+kurtosis(dd_hi_ToU$energy_sum, na.rm = FALSE, method = c("excess", "moment", "fisher"))
+kurtosis(dd_hi_ToU$energy_sum)
+kurtosis(dd_hi_ToU$energy_max)
 
 
-dd_hi_std <- dbGetQuery(dbConn, "SELECT * FROM daily_dataset
+dd_hi_Std <- dbGetQuery(dbConn, "SELECT * FROM daily_dataset
 LEFT JOIN informations_households ON daily_dataset.LCLid = informations_households.LCLid
 WHERE informations_households.stdorToU = 'Std' AND energy_sum IS NOT NULL; ")
+class(dd_hi_Std)
 
-median(dd_hi_std$energy_sum)
-median(dd_hi_std$energy_max)
-
-
-
-# import the left join of the half hourly and household
-# information tables
-
-str(hh_hi)
+median(dd_hi_Std$energy_sum)
+median(dd_hi_Std$energy_max)
+kurtosis(dd_hi_Std$energy_sum)
+kurtosis(dd_hi_Std$energy_max)
 
 
 
-# I got the value of the mean of the daily energy sum
-# for the treatment group from an SQL query
-avgdes_t <- 9.5
-# Likewise for its standard deviation
-stddes_t <- 8.08
-# Sample size for all of the daily readings of the treatment group
-n1 <- 706031
-# I got the value of the mean of the daily energy sum
-# for the control group from another SQL query
-avgdes_c <- 10.28
-# Same for its standard deviation
-stddes_c <- 9.37
-# Sample size for all of the daily readings of the control group
-n2 <- 2804402
-
-# manually perform a difference in means t-test on the daily energy sum
-# for the treatment and control groups
-dmdes = avgdes_t - avgdes_c
-dmdes
-# calculate the standard error 
-se = sqrt(((stddes_t**2)/n1) + ((stddes_c**2)/n2))
-se
-t = dmdes/se
-t
-# -70.1 is statistically significant at any conceivable threshold. 
 
 
-# Repeat the above process for the daily maximum energy
-# usage to see if time of use pricing reduced the acuteness
-# of peak daily energy consumption among Londoners.
-avgmax_t <- 0.79    # mean max daily energy use in the treatment group 
-stdmax_t <- 0.60    # the standard deviation of max energy use in a half hour period
 
-# mean max daily energy use in the control group
-avgmax_c <- 0.85
-stdmax_c <- 0.68
 
-# calculate the standard error
-se2 = sqrt(((stdmax_t**2)/n1) + ((stdmax_c**2)/n2))
-se2
-# manually perform a difference in means t-test on the max daily energy
-# use between the treatment and control groups
-dm2 = avgmax_t - avgmax_c
-dm2
-t_max = dm2/se2
-t_max
-# -73.0 is statistically significant at any conceivable threshold. 
+ 
 
 
 # also run a Wilcoxon Rank Sum Test on the sum of daily energy 
 # use because the daily usage data appears to have 
 # much more variation than a Gaussian distribution
 attach(dd_hi)
-wilcox.test(energy_sum ~ stdorToU, data = dd_hi, alternative = "greater")
+wilcox.test(energy_sum ~ stdorToU, mu = 0, alternative = "greater", 
+            conf.int = T, paired = FALSE)
+
+wilcox.test(energy_sum ~ stdorToU, mu = 0, alternative = "less", 
+            conf.int = TRUE, paired = F)
+
+# calculate the Wilcoxon effect size of the difference
+dd_hi %>% wilcox_effsize(energy_sum ~ stdorToU, paired = FALSE,
+                         alternative = "greater", mu = 0, ci = FALSE,
+                         conf.level = 0.95, ci.type = "perc", nboot = 1000)
 
 
 # also run a Wilcoxon Rank Sum Test on the max amount of daily energy usage
 wilcox.test(energy_max ~ stdorToU, data = dd_hi, alternative = "greater")
 
+wilcox.test(energy_max ~ stdorToU, mu = 0, alternative = "greater",
+            conf.int = TRUE, paired = FALSE)
 
 # calculate the Wilcoxon effect size of the difference
-dd_hi %>% wilcox_effsize(energy_sum ~ stdorToU, paired = FALSE,
+dd_hi %>% wilcox_effsize(energy_max ~ stdorToU, paired = FALSE,
                alternative = "greater", mu = 0, ci = FALSE,
                conf.level = 0.95, ci.type = "perc", nboot = 1000)
 
@@ -239,58 +188,97 @@ dd_hi %>% wilcox_effsize(energy_sum ~ stdorToU, paired = FALSE,
 # str(entity_FEs)
 # head(entity_FEs)
 
-attach(dd_hi)
+dd_hi2 <- dd_hi[, c(2:9, 12:37)]
+head(dd_hi2)
+str(dd_hi2)
+attach(dd_hi2)
 Total_Energy_FE <- plm(formula = energy_sum ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
-                  Adversity_dummy + AcornU_dummy + AcornA_dummy + AcornB_dummy 
+                  Adversity_dummy + AcornA_dummy + AcornB_dummy 
                   + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy 
                   + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
                   + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
                   + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
-                   data = dd_hi, model = "within", index = "day")
+                   data = dd_hi2, model = "within", index = "day")
 summary(Total_Energy_FE)
 
+
+Total_Energy_FE2 <- plm(formula = energy_sum ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
+                            Adversity_dummy + AcornA_dummy + AcornB_dummy + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
+                        + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
+                        + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
+                        data = dd_hi2, model = "within", index = c("id", "day"))
+summary(TTotal_Energy_FE2)
+
+
+
+
 Total_Energy_RE <- plm(formula = energy_sum ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
-                  Adversity_dummy + AcornU_dummy + AcornA_dummy + AcornB_dummy + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
-              + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
-              + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
-              data = dd_hi, model = "random", index = c("LCLid", "day"))
+                           Adversity_dummy + AcornA_dummy + AcornB_dummy 
+                       + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy 
+                       + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
+                       + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
+                       + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
+                       data = dd_hi2, model = "random", index = "day")
 summary(Total_Energy_RE)
 
 
 
+
+Total_Energy_RE2 <- plm(formula = energy_sum ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
+                           Adversity_dummy + AcornA_dummy + AcornB_dummy + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
+                       + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
+                       + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
+                       data = dd_hi2, model = "random", index = c("id", "day"))
+summary(Total_Energy_RE2)
+
+
+# run a Hausman test to see whether I should use fixed effects or random effects
+phtest(Total_Energy_FE, Total_Energy_RE)
+
+
+
+
+
+
+# fun an FE and a RE on the data but with energy_max as the dependent variable 
 EnergyMax_FE <- plm(formula = energy_max ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
-                        Adversity_dummy + AcornU_dummy + AcornA_dummy + AcornB_dummy 
+                        Adversity_dummy + AcornA_dummy + AcornB_dummy 
                     + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy 
                     + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
                     + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
                     + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
-                    data = dd_hi, model = "within", index = "day")
+                    data = dd_hi2, model = "within", index = "day")
 summary(EnergyMax_FE)
 
-EnergyMax_RE <- plm(formula = energy_max ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
-                         Adversity_dummy + AcornU_dummy + AcornA_dummy + AcornB_dummy 
-                     + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy 
-                     + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
-                     + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
-                     + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
-                     data = dd_hi, model = "random", index = c("LCLid", "day"))
-summary(EnergyMax_RE)
+rm(Total_Energy_FE)
 
 
 # estimate a random effects regression model
-attach(dd_hi)
-ToU_RE <- plm(formula = energy_sum ~ ToU_dummy + Affluent_dummy + 
-                  Comfortable_dummy + Adversity_dummy + AcornU_dummy + 
+attach(dd_hi2)
+
+EnergyMax_RE <- plm(formula = energy_max ~ ToU_dummy + Affluent_dummy + 
+                  Comfortable_dummy + Adversity_dummy + 
                   AcornA_dummy + AcornB_dummy + AcornC_dummy + AcornD_dummy +
                   AcornE_dummy + AcornF_dummy + AcornG_dummy + AcornH_dummy +
                   AcornI_dummy + AcornJ_dummy + AcornK_dummy + AcornL_dummy +
                   AcornM_dummy + AcornN_dummy + AcornO_dummy + AcornP_dummy +
-                  AcornQ_dummy, data = dd_hi, index = "day", model = "random")
-summary(ToU_RE)
+                  AcornQ_dummy, data = dd_hi2, index = "day", model = "random")
+summary(EnergyMax_RE)
+
+
+
+EnergyMax_RE2 <- plm(formula = energy_max ~ ToU_dummy + Affluent_dummy + Comfortable_dummy + 
+                        Adversity_dummy + AcornA_dummy + AcornB_dummy 
+                    + AcornC_dummy + AcornD_dummy + AcornE_dummy + AcornF_dummy 
+                    + AcornG_dummy + AcornH_dummy + AcornI_dummy + AcornJ_dummy 
+                    + AcornK_dummy + AcornL_dummy + AcornM_dummy + AcornN_dummy 
+                    + AcornO_dummy + AcornP_dummy + AcornQ_dummy, 
+                    data = dd_hi2, model = "random", index = c("id", "day"))
+summary(EnergyMax_RE2)
 
 
 # run a Hausman test to see whether I should use fixed effects or random effects
-phtest(ToU_FE, ToU_RE)
+phtest(EnergyMax_FE, EnergyMax_RE)
 
 
 
