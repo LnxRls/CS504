f0a2190f77995259f0bba6dd710839b69d5b1e13
Spencer <spencermstarr@hotmail.com>
2021-04-30 17:00:21 -0700

    Add files via upload

--- Spencer's data visualizations R code.R
+++ Spencer's data visualizations R code.R
@@ -21,7 +21,8 @@ dbListTables(dbConn)
 
 
 # import a left join of the daily dataset and household
-# information tables into RStudio as a data frame
+# information tables into RStudio as a data frame that
+# only includes records during 2013
 dd_hi <- dbGetQuery(dbConn, "SELECT * FROM daily_dataset
 LEFT JOIN informations_households ON daily_dataset.LCLid = informations_households.LCLid
 WHERE day BETWEEN '2013-01-01' AND '2013-12-31'; ")
@@ -32,6 +33,11 @@ dd_hi$day <- ymd(dd_hi$day)
 # add a numerical ID column
 dd_hi$id <- substr(dd_hi$LCLid, 4, 9)
 dd_hi$id
+dd_hi$id <- as.integer(dd_hi$id)
+
+# convert the stdorToU variable/column which is currently a 
+# chr variable to a factor
+dd_hi$stdorToU <- as.factor(dd_hi$stdorToU)
 
 # add a dummy variable column for time of use houses
 dd_hi$ToU_dummy <- ifelse(dd_hi$stdorToU == "ToU", 1, 0)
@@ -148,18 +154,21 @@ ggplot(data = hhblocks_avg) + geom_col(aes(y = mean_1200am)) + theme_light()
     
 # create a Q-Q plot of the daily_dataset for the energy_sum versus
 # whether a house is on time of use pricing or not
-qqplot(dd_hi$energy_sum[dd_hi$ToU_dummy == 1],
-       dd_hi$energy_sum[dd_hi$ToU_dummy == 0],
-       xlab = 'Houses on Dynamic Time of Use Energy Rate',
-       ylab = 'Houses on the Standard Flat Energy Rate',
-       main = 'Q-Q Plot of Total Daily Energy Usage vs Time of Use Pricing')
+# qqplot(dd_hi$energy_sum[dd_hi$ToU_dummy == 1],
+#       dd_hi$energy_sum[dd_hi$ToU_dummy == 0],
+#       xlab = 'Houses on Dynamic Time of Use Energy Rate',
+#       ylab = 'Houses on the Standard Flat Energy Rate',
+#       main = 'Q-Q Plot of Total Daily Energy Usage vs Time of Use Pricing')
 
 
-qqnorm(dd_hi$energy_sum, main = 'Q-Q Plot of the Observed Distribution of energy_sum vs a Gaussian Distribution')
+
+# create a Q-Q plot of the distribution of the energy_sum column in the 
+# daily_dataset table for all households
+qqnorm(dd_hi$energy_sum, main = 'Q-Q Plot of the Observed Distribution of energy_sum \nvs a Theoretical Gaussian')
 qqline(dd_hi$energy_sum)
 
 
-#create a Q-Q plot of the distribution of the energy_sum column in the 
+# create a Q-Q plot of the distribution of the energy_sum column in the 
 # daily_dataset table for houses on time of use pricing 
 # versus the Normal distribution
 qqnorm(dd_hi$energy_sum[dd_hi$ToU_dummy == 1],
@@ -176,3 +185,15 @@ qqnorm(dd_hi$energy_sum[dd_hi$ToU_dummy == 0],
        ylab = 'Sample Quantiles', 
        main = 'Q-Q Plot of the Distribution of energy_sum for Std Houses vs a Gaussian')
 qqline(dd_hi$energy_sum[dd_hi$ToU_dummy == 0])
+
+
+
+
+
+
+
+# create a Q-Q plot of the distribution of the energy_max field 
+# in the daily_dataset table for all households
+qqnorm(dd_hi$energy_max, 
+main = 'Q-Q Plot of the Observed Distribution of energy_max \nvs a Theorical Gaussian Distribution')
+qqline(dd_hi$energy_max)
